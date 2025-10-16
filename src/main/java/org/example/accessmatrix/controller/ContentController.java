1382247b05630ebd5a4197f4878c3d4596672668
MakDerive <kirillandmax21@gmail.com>
2025-10-16 16:36:40 +0300

    Test init

--- src/main/java/org/example/accessmatrix/controller/ContentController.java
+++ src/main/java/org/example/accessmatrix/controller/ContentController.java
@@ -2,10 +2,16 @@ package org.example.accessmatrix.controller;
 
 import lombok.AllArgsConstructor;
 import org.example.accessmatrix.service.MatrixService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.*;
 
 @Controller
@@ -29,6 +35,17 @@ public class ContentController {
         model.addAttribute("matrixUsers",users);
         model.addAttribute("matrix",matrix);
         return "index";
+    }
+
+    @GetMapping(value = "/test.txt", produces = "text/plain")
+    public ResponseEntity<String> getTextFile() {
+        try {
+            Resource resource = new ClassPathResource("/static/test.txt");
+            String content = new String(Files.readAllBytes(resource.getFile().toPath()));
+            return ResponseEntity.ok(content);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found");
+        }
     }
 
 }
